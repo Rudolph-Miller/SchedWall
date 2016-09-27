c9c596a407ad7be5518b61caa886324bd94d31c4
Rudolph-Miller <me@rudolph-miller.com>
2016-09-27 17:34:19 +0900

    Add NavigatorIOS and ListEvents

--- index.ios.js
+++ index.ios.js
@@ -6,47 +6,46 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry, View, NavigatorIOS, StyleSheet
 } from 'react-native';
+import { ListEvents } from './src/components';
+
+function wrap(klass) {
+  return (props) => {
+    return (
+      <View style={styles.wrapper}>
+        {React.createElement(klass, props)}
+      </View>
+    );
+  };
+}
 
 class SchedWall extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        ref='nav'
+        initialRoute={{
+          component: wrap(ListEvents),
+          title: 'SchedWall'
+        }}
+        barTintColor='#FF5722'
+        tintColor='#fff'
+        titleTextColor='#fff'
+        style={styles.container} />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  wrapper: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'column',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    paddingTop: 64
   },
 });
 
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export ListEvents from './ListEvents';
